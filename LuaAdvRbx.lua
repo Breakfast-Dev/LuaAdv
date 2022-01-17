54ed54fd0382735e0b494e5eab3f3ee2688ad996
Breakfast-Dev <thdwns0118@naver.com>
2022-01-17 16:16:25 +0900

    Update LuaAdvRbx.lua

--- LuaAdvRbx.lua
+++ LuaAdvRbx.lua
@@ -270,9 +270,9 @@ global. Error = table.object({
   end;
   print = function(self)
     print(
-      "\nLuaAdv[Error] | " .. self.Name .. ": " .. self.Description .. "\n"  ..
-      string.rep(" ", #self.Name + 11) .. "at   -> " .. tostring((self.Context or { Name = "Unknown" }).Name .. "\n" .. 
-      string.rep(" ", #self.Name + 11) .. "line ->" .. self.Line) .. "\n\n" .. 
+      "\n ! LuaAdv[Error] | " .. self.Name .. ": " .. self.Description .. "\n"  ..
+      string.rep(" ", #self.Name + 14) .. "at   -> " .. tostring((self.Context or { Name = "Unknown" }).Name .. "\n" .. 
+      string.rep(" ", #self.Name + 14) .. "line ->" .. self.Line) .. "\n\n" .. 
       " * This is caught error by LuaAdv\n\n"
     )
   end;
